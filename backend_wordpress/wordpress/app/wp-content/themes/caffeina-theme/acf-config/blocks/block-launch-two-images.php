d4a77979d564325b0d31a05e2f095177e5ce41c7
Giuseppe Gatto <giuseppe.gatto@caffeina.com>
2021-11-05 14:56:55 +0000

    add two images block

---
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/acf-config/blocks/block-launch-two-images.php
@@ -0,0 +1,25 @@
+<?php
+
+add_action('acf/init', 'lb_init_block_launch_two_images');
+function lb_init_block_launch_two_images() {
+
+    if( function_exists('acf_register_block_type') ) {
+
+        acf_register_block_type(array(
+            'name'              => 'block-launch-two-images',
+            'title'             => __('block-launch-two-images'),
+            'description'       => __('A custom block-launch-two-images block.'),
+            'render_template'   => 'gutenberg-blocks/block-launch-two-images.php',
+            'category'          => 'caffeina-theme',
+            'icon'              => 'admin-comments',
+            'keywords'          => array('block-launch-two-images'),
+            'example'  => array(
+                'attributes' => array(
+                    'data' => array(
+                        'is_preview' => true
+                    )
+                )
+            )
+        ));
+    }
+}
---
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/gutenberg-blocks/block-launch-two-images.php
@@ -0,0 +1,65 @@
+<?php
+require_once(__DIR__.'/baseBlock.php');
+include get_template_directory() . '/functions/setup/composer-packages.php';
+use gutenbergBlocks\BaseBlock;
+$block_launch_two_images = new BaseBlock($block,$twig);
+$payload = [
+    'imagesLeft' => [
+        'original' => get_field('lb_block_launch_two_images_image_left'),
+        'large' => get_field('lb_block_launch_two_images_image_left'),
+        'medium' => get_field('lb_block_launch_two_images_image_left'),
+        'small' => get_field('lb_block_launch_two_images_image_left')
+    ],
+    'imagesRight' => [
+        'original' => get_field('lb_block_launch_two_images_image_right'),
+        'large' => get_field('lb_block_launch_two_images_image_right'),
+        'medium' => get_field('lb_block_launch_two_images_image_right'),
+        'small' => get_field('lb_block_launch_two_images_image_right')
+    ],
+    'infobox' => [
+        'side' => get_field('lb_block_launch_two_images_side'),
+        'tagline' => get_field('lb_block_launch_two_images_tagline'),
+        'title' => get_field('lb_block_launch_two_images_title'),
+        'subtitle' => get_field('lb_block_launch_two_images_subtitle'),
+        'paragraph' => get_field('lb_block_launch_two_images_paragraph'),
+        'button' => get_field('lb_block_launch_two_images_btn')
+    ]
+];
+$block_launch_two_images->setContext($payload);
+$block_launch_two_images->render();
+
+
+/**
+ *  launch two images Block Template
+ */
+//??
+
+// Carousel data
+
+
+
+
+
+
+// Payload
+// 'imagesLeft' => [
+//     'original' => get_field('lb_block_launch_two_images_image_left'),
+//     'large' => get_field('lb_block_launch_two_images_image_left'),
+//     'medium' => get_field('lb_block_launch_two_images_image_left'),
+//     'small' => get_field('lb_block_launch_two_images_image_left')
+// ],
+// 'imagesRight' => [
+//     'original' => get_field('lb_block_launch_two_images_image_right'),
+//     'large' => get_field('lb_block_launch_two_images_image_right'),
+//     'medium' => get_field('lb_block_launch_two_images_image_right'),
+//     'small' => get_field('lb_block_launch_two_images_image_right')
+// ],
+// 'infobox' => [
+//     'side' => get_field('lb_block_launch_two_images_side'),
+//     'tagline' => get_field('lb_block_launch_two_images_tagline'),
+//     'title' => get_field('lb_block_launch_two_images_title'),
+//     'subtitle' => get_field('lb_block_launch_two_images_subtitle'),
+//     'paragraph' => get_field('lb_block_launch_two_images_paragraph'),
+//     'button' => get_field('lb_block_launch_two_images_btn')
+// ]
+
