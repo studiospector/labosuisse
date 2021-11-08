cbc978b88cf613d3c3444a679399f85498fdbac4
Giuseppe Gatto <giuseppe.gatto@caffeina.com>
2021-11-08 07:58:32 +0000

    remove all of comments

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/functions.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/functions.php
@@ -41,41 +41,11 @@ if (!defined('CF_BUILD_LIB_URI')) {
 }
 
 
-
-// Composer autoload
-// require_once( __DIR__ . '/vendor/autoload.php' );
-
 // *** THEME SETUP ***
 include __DIR__.'/functions/setup/composer-packages.php';
 include __DIR__.'/functions/setup/theme-setup.php';
 include __DIR__.'/functions/setup/theme-assets.php';
-// add_theme_support('post-thumbnails');
-// add_filter('use_block_editor_for_post', '__return_false');
-// include __DIR__.'/functions/setup/configure-wp-editor.php';
-// include __DIR__.'/functions/setup/disable-comments.php';
-// include __DIR__.'/functions/setup/disable-front-page-redirect.php';
-// include __DIR__.'/functions/setup/mime-types.php';
-// include __DIR__.'/functions/setup/pagination-custom-permalink.php';
-// include __DIR__.'/functions/setup/transform-permalink.php';
-// include __DIR__.'/functions/setup/virtual-pages.php';
 
-// *** THEME API ***
-// include __DIR__.'/functions/api/cpt.php';
-// include __DIR__.'/functions/api/rest-headless-cms.php';
-// include __DIR__.'/functions/api/menus.php';
-// include __DIR__.'/functions/api/options.php';
-
-// *** THEME UTILS ***
-// include __DIR__.'/functions/utils/get-meta-tags-from-string.php';
-// include __DIR__.'/functions/utils/json-response.php';
-// include __DIR__.'/functions/utils/soft-trim.php';
-// include __DIR__.'/functions/utils/transform-image-to-figure.php';
-// include __DIR__.'/functions/utils/truncate-html.php';
-
-// ACF Config
-// include __DIR__.'/acf-config/acf-field-cf-book.php';
-// include __DIR__.'/acf-config/acf-field-cf-loan.php';
-// include __DIR__.'/acf-config/acf-field-cf-review.php';
 
 // ACF Config JSON save point
 add_filter('acf/settings/save_json', 'labo_acf_json_save_point');
