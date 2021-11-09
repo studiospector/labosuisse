d39a3adf912b11495abb6925e44053c1ee8b1e7e
Stefano Iachetta <stefano.iachetta@hotmail.it>
2021-11-09 12:08:14 +0100

    fix: vendor autoload path

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/composer-packages.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/composer-packages.php
@@ -1,7 +1,7 @@
 <?php
 
-if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
-    require_once(__DIR__ . '/../../vendor/autoload.php');
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../vendor/autoload.php');
 
     include  __DIR__ . '/twig-engine.php';
 }
