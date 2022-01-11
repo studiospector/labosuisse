291a0e637e48fcfbd4db7d3b8244f238a7f495cb
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-01-11 16:51:25 +0100

    feat: add blocks to product page

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/single-product.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/single-product.php
@@ -168,7 +168,7 @@ $context['miniatures'] = [
     'variants' =>['full'], // default, full
 ];
 
-// Miniatures
+// Two cards
 $context['two_cards'] = [
     'infobox' => [
         'subtitle' => 'Individua il trattamento adatto a te',
@@ -209,6 +209,35 @@ $context['two_cards'] = [
                 ]
             ]
         ]
+    ]
+];
+
+// Image and card
+$context['image_and_card'] = [
+    'images' => [
+        'original' => '/assets/images/banner-img.jpg',
+        'large' => '/assets/images/banner-img.jpg',
+        'medium' => '/assets/images/banner-img.jpg',
+        'small' => '/assets/images/banner-img.jpg'
+    ],
+    'card' => [
+        'images' => [
+            'original' => '/assets/images/carousel-hero-img.jpg',
+            'large' => '/assets/images/carousel-hero-img.jpg',
+            'medium' => '/assets/images/carousel-hero-img.jpg',
+            'small' => '/assets/images/carousel-hero-img.jpg'
+        ],
+        'infobox' => [
+            'subtitle' => 'La tecnologia dietro l’efficacia',
+            'paragraph' => 'Grazie alla Tecnologia Transdermica (Swiss Patent CH 711 466) - ispirata alla metodologia della medicina estetica e brevettata nel 2015 - Labo supera le frontiere della scienza dermo-cosmetica divenendo la prima azienda a sviluppare una nuova tecnica di penetrazione dei principi attivi, senza iniezioni, attraverso epidermide e derma.',
+            'cta' => [
+                'url' => '#',
+                'title' => 'Scopri di più',
+                'iconEnd' => [ 'name' => 'arrow-right' ],
+                'variants' => ['quaternary']
+            ]
+        ],
+        'variants' => ['type-7']
     ]
 ];
 
