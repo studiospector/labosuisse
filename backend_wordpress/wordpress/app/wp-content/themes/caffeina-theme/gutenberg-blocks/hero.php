db37c998ea686033470da75e0c2e650928926c41
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-01-13 15:18:49 +0100

    fix: hero block

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/gutenberg-blocks/hero.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/gutenberg-blocks/hero.php
@@ -7,23 +7,18 @@ $block_hero = new BaseBlock($block);
 
 $payload = [
     'images' => [
-        'original' => get_sub_field('lb_block_hero_img'),
-        'large' => get_sub_field('lb_block_hero_img'),
-        'medium' => get_sub_field('lb_block_hero_img'),
-        'small' => get_sub_field('lb_block_hero_img')
+        'original' => get_field('lb_block_hero_img'),
+        'large' => get_field('lb_block_hero_img'),
+        'medium' => get_field('lb_block_hero_img'),
+        'small' => get_field('lb_block_hero_img')
     ],
-    'infoboxPosX' => get_sub_field('lb_block_hero_infoboxposx'),
-    'infoboxPosY' => get_sub_field('lb_block_hero_infoboxposy'),
-    'container' => get_sub_field('lb_block_hero_container'),
-    'variants' => get_sub_field('lb_block_hero_variants'),
-
+    'infoboxPosX' => get_field('lb_block_hero_infoboxposx'),
+    'infoboxPosY' => get_field('lb_block_hero_infoboxposy'),
+    'container' => get_field('lb_block_hero_container'),
+    'variants' => [get_field('lb_block_hero_variants')],
 ];
 
 // $this->context['data'] = array_merge($this->context['data'],$infobox);
 $block_hero->setContext($payload);
 $block_hero->addInfobox();
 $block_hero->render();
-
-
-
-
