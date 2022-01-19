f785ca78fe49c1f0cb33eb543633c2ae6b0c8793
Giuseppe <giuseppe.gatto@caffeina.com>
2022-01-19 17:34:36 +0100

    fix components  and modify sigle product

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/gutenberg-blocks/block-image-card.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/gutenberg-blocks/block-image-card.php
@@ -1,5 +1,8 @@
 <?php
 require_once(__DIR__.'/classes/imageCard.php');
 use gutenbergBlocks\BaseBlock;
-$block_image_card = new ImageCard($block,null);
+$block_image_card = new ImageCard($block,'block-image-card');
+// echo "<pre>";
+// var_dump($block_image_card->context);
+//  die;
 $block_image_card->render();
--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/single-product.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/single-product.php
@@ -34,229 +34,36 @@ $related_limit = wc_get_loop_prop('columns');
 $related_ids = wc_get_related_products($context['post']->id, $related_limit);
 $context['related_products'] = Timber::get_posts($related_ids);
 
-// Banner
-// $context['hero'] = [
-//     'images' => [
-//         'original' => get_template_directory_uri() . '/assets/images/hero-crescina-right.jpg',
-//         'large' => get_template_directory_uri() . '/assets/images/hero-crescina-right.jpg',
-//         'medium' => get_template_directory_uri() . '/assets/images/hero-crescina-right.jpg',
-//         'small' => get_template_directory_uri() . '/assets/images/hero-crescina-right.jpg'
-//     ],
-//     'infoboxPosX' => 'left',
-//     'infoboxPosY' => 'center',
-//     'infobox' => [
-//         'subtitle' => 'La risposta specifica al problema del diradamento',
-//         'paragraph' => 'Un concentrato di innovazione dedicata alla Ri-Crescita naturale dei capelli.',
-//     ],
-//     'container' => true,
-//     'variants' => ['small']
-// ];
+// Banner -> hero
 require_once(WP_CONTENT_DIR.'/themes/caffeina-theme/gutenberg-blocks/classes/hero.php');
 $block_hero = new Hero(null,'hero');
-
 $context['hero'] = $block_hero->payload;
 
-// Banner alternate
-// $context['banner_alternate'] = [
-//     'images' => [
-//         'original' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-//         'large' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-//         'medium' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-//         'small' => get_template_directory_uri() . '/assets/images/banner-img.jpg'
-//     ],
-//     'infobox' => [
-//         'tagline' => 'Dosaggi e formati',
-//         'subtitle' => 'Sappiamo bene che ogni persona è unica',
-//         'paragraph' => 'Per questo, abbiamo studiato formule specifiche - per uomo e per donna - in concentrazioni diversificate dei principi attivi adatte a trattare i differenti gradi di diradamento, da quello iniziale a quello avanzato di calvizie, anche dovuto ad Alopecia Androgenetica.',
-//         'variants' => ['alternate'],
-//         'cta' =>[
-//             'url' =>'#',
-//             'title' =>'Lorem Ipsum',
-//             'iconEnd' => [ 'name' => 'arrow-right' ],
-//             'variants' =>['quaternary']
-//         ]
-//     ],
-//     'imageBig' => false,
-//     'variants' => ['infobox-right', 'infobox-centered'], // infobox-left, infobox-right AND infobox-fullheight, infobox-centered, infobox-bottom
-// ];
 require_once(WP_CONTENT_DIR.'/themes/caffeina-theme/gutenberg-blocks/classes/bannerAlternate.php');
 $block_banner_alternate = new BannerAlternate(null,'banner_alternate');
 
 $context['banner_alternate'] = $block_banner_alternate->payload;
 
 // Number List
-$context['number_list'] = [
-    'images' => [
-        'original' => get_template_directory_uri() . '/assets/images/carousel-hero-img-2.jpg',
-        'large' => get_template_directory_uri() . '/assets/images/carousel-hero-img-2.jpg',
-        'medium' => get_template_directory_uri() . '/assets/images/carousel-hero-img-2.jpg',
-        'small' => get_template_directory_uri() . '/assets/images/carousel-hero-img-2.jpg'
-    ],
-    'numbersList' => [
-        'title' => 'Tre consigli utili',
-        'list' => [
-            [
-                'title' => '',
-                'text' => 'Si consiglia un trattamento di almeno 2 mesi.<br>Può essere ripetuto più volte l’anno.',
-            ],
-            [
-                'title' => '',
-                'text' => 'Utilizza Crescina sul cuoio capelluto<br>completamente integro, senza escoriazioni.',
-            ],
-            [
-                'title' => '',
-                'text' => 'Regola le tue abitudini di detersione:<br>lava i capelli almeno due volte a settimana.',
-            ],
-        ],
-        'variants' => ['vertical']
-    ]
-];
+require_once(WP_CONTENT_DIR.'/themes/caffeina-theme/gutenberg-blocks/classes/numberListImage.php');
+$block_numbers = new NumberListImage(null, 'number-list-with-image');
+$context['number_list'] = $block_numbers->payload;
 
-// Miniatures
-$context['miniatures'] = [
-    'infobox' =>[
-        'title' =>'Come applicare crescina',
-        'paragraph' =>'Applica 1 fiala al giorno per 5 giorni consecutivi su capelli asciutti (ad esempio i primi<br>5 giorni della settimana) con 2 giorni di pausa. Non risciacquare. ',
-        'cta' =>[
-            'url' =>'#',
-            'title' =>'Guarda i tutorial',
-            'variants' =>['tertiary']
-        ]
-    ],
-    'items' =>[
-        [
-            'images' =>[
-                'original' =>get_template_directory_uri() . '/assets/images/love-labo-img-1.jpg',
-                'large' =>get_template_directory_uri() . '/assets/images/love-labo-img-1.jpg',
-                'medium' =>get_template_directory_uri() . '/assets/images/love-labo-img-1.jpg',
-                'small' =>get_template_directory_uri() . '/assets/images/love-labo-img-1.jpg'
-            ],
-            'text' =>'Prendere dall’alloggio l’apposito rompifiala in plastica rigida incluso nella confezione.'
-        ],
-        [
-            'images' =>[
-                'original' =>get_template_directory_uri() . '/assets/images/love-labo-img-2.jpg',
-                'large' =>get_template_directory_uri() . '/assets/images/love-labo-img-2.jpg',
-                'medium' =>get_template_directory_uri() . '/assets/images/love-labo-img-2.jpg',
-                'small' =>get_template_directory_uri() . '/assets/images/love-labo-img-2.jpg'
-            ],
-            'text' =>'Inserire il rompifiala sulla fiala, semplicemente appoggiandolo. Stringere tra le dita il rompifiala e rompere la parte alta della fiala facendo una leggera ma decisa pressione.'
-        ],
-        [
-            'images' =>[
-                'original' =>get_template_directory_uri() . '/assets/images/love-labo-img-3.jpg',
-                'large' =>get_template_directory_uri() . '/assets/images/love-labo-img-3.jpg',
-                'medium' =>get_template_directory_uri() . '/assets/images/love-labo-img-3.jpg',
-                'small' =>get_template_directory_uri() . '/assets/images/love-labo-img-3.jpg'
-            ],
-            'text' =>'Prendere dall’alloggio il dosatore con beccuccio.'
-        ],
-        [
-            'images' =>[
-                'original' =>get_template_directory_uri() . '/assets/images/love-labo-img-4.jpg',
-                'large' =>get_template_directory_uri() . '/assets/images/love-labo-img-4.jpg',
-                'medium' =>get_template_directory_uri() . '/assets/images/love-labo-img-4.jpg',
-                'small' =>get_template_directory_uri() . '/assets/images/love-labo-img-4.jpg'
-            ],
-            'text' =>'Inserire l’apposito dosatore con beccuccio sulla fiala aperta.'
-        ],
-        [
-            'images' =>[
-                'original' =>get_template_directory_uri() . '/assets/images/love-labo-img-5.jpg',
-                'large' =>get_template_directory_uri() . '/assets/images/love-labo-img-5.jpg',
-                'medium' =>get_template_directory_uri() . '/assets/images/love-labo-img-5.jpg',
-                'small' =>get_template_directory_uri() . '/assets/images/love-labo-img-5.jpg'
-            ],
-            'text' =>'Togliere il cappuccio del dosatore.'
-        ],
-        [
-            'images' =>[
-                'original' =>get_template_directory_uri() . '/assets/images/love-labo-img-6.jpg',
-                'large' =>get_template_directory_uri() . '/assets/images/love-labo-img-6.jpg',
-                'medium' =>get_template_directory_uri() . '/assets/images/love-labo-img-6.jpg',
-                'small' =>get_template_directory_uri() . '/assets/images/love-labo-img-6.jpg'
-            ],
-            'text' =>'Applicare il preparato sul cuoio capelluto pulito e asciutto, riga per riga, insistendo sulle zone più diradate, facendo attenzione a non farlo colare sul viso. Far penetrare con leggero massaggio.'
-        ],
-    ],
-    'variants' =>['full'], // default, full
-];
+// Miniatures -> love labo
+require_once(WP_CONTENT_DIR.'/themes/caffeina-theme/gutenberg-blocks/classes/loveLabo.php');
+$block_love_labo = new LoveLabo(null, "block-love-labo");
+$context['miniatures'] = $block_love_labo->payload;
 
 //Two cards
-$context['two_cards'] = [
-    'infobox' => [
-        'subtitle' => 'Individua il trattamento adatto a te',
-        'paragraph' => 'Per conoscere il tuo grado di diradamento e il dosaggio più indicato per te,<br>consulta la tabella.',
-    ],
-    'cards' => [
-        [
-            'images' => [
-                'original' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-                'large' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-                'medium' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-                'small' => get_template_directory_uri() . '/assets/images/banner-img.jpg'
-            ],
-            'infobox' => [
-                'subtitle' => 'Scala di diradamento uomo',
-                'cta' => [
-                    'url' => '#',
-                    'title' => 'Visualizza la scala',
-                    'iconEnd' => [ 'name' => 'arrow-right' ],
-                    'variants' => ['quaternary']
-                ]
-            ]
-        ],
-        [
-            'images' => [
-                'original' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-                'large' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-                'medium' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-                'small' => get_template_directory_uri() . '/assets/images/banner-img.jpg'
-            ],
-            'infobox' => [
-                'subtitle' => 'Scala di diradamento donna',
-                'cta' => [
-                    'url' => '#',
-                    'title' => 'Visualizza la scala',
-                    'iconEnd' => [ 'name' => 'arrow-right' ],
-                    'variants' => ['quaternary']
-                ]
-            ]
-        ]
-    ]
-];
-// require_once(WP_CONTENT_DIR.'/themes/caffeina-theme/gutenberg-blocks/classes/launchTwoCards.php');
-// $block_two_cards = new LaunchTwoCards(null,'two_cards');
+require_once(WP_CONTENT_DIR.'/themes/caffeina-theme/gutenberg-blocks/classes/launchTwoCards.php');
+$block_launch_two_cards = new LaunchTwoCards(null, "block-two-cards");
+$context['two_cards'] = $block_launch_two_cards->payload;
 
-// $context['two_cards'] = $block_two_cards->payload;
 // Image and card
-$context['image_and_card'] = [
-    'images' => [
-        'original' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-        'large' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-        'medium' => get_template_directory_uri() . '/assets/images/banner-img.jpg',
-        'small' => get_template_directory_uri() . '/assets/images/banner-img.jpg'
-    ],
-    'card' => [
-        'images' => [
-            'original' => get_template_directory_uri() . '/assets/images/carousel-hero-img.jpg',
-            'large' => get_template_directory_uri() . '/assets/images/carousel-hero-img.jpg',
-            'medium' => get_template_directory_uri() . '/assets/images/carousel-hero-img.jpg',
-            'small' => get_template_directory_uri() . '/assets/images/carousel-hero-img.jpg'
-        ],
-        'infobox' => [
-            'subtitle' => 'La tecnologia dietro l’efficacia',
-            'paragraph' => 'Grazie alla Tecnologia Transdermica (Swiss Patent CH 711 466) - ispirata alla metodologia della medicina estetica e brevettata nel 2015 - Labo supera le frontiere della scienza dermo-cosmetica divenendo la prima azienda a sviluppare una nuova tecnica di penetrazione dei principi attivi, senza iniezioni, attraverso epidermide e derma.',
-            'cta' => [
-                'url' => '#',
-                'title' => 'Scopri di più',
-                'iconEnd' => [ 'name' => 'arrow-right' ],
-                'variants' => ['quaternary']
-            ]
-        ],
-        'variants' => ['type-7']
-    ]
-];
+require_once(WP_CONTENT_DIR.'/themes/caffeina-theme/gutenberg-blocks/classes/imageCard.php');
+$block_image_card = new ImageCard(null,'block-image-card');
+$context['image_and_card'] = $block_image_card->payload;
+
 
 // Routine
 $context['routine'] = [
