7c0d3e72665a016e4a4798e4be122201e04827c2
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-02-01 16:14:29 +0100

    fix: carousel centered centered block

--- frontend_bundler/client/app/scripts/components/CarouselCentered/index.js
+++ frontend_bundler/client/app/scripts/components/CarouselCentered/index.js
@@ -12,25 +12,27 @@ export default class CarouselCentered extends Component {
     constructor({ options, ...props }) {
         super({ ...props, ui })
 
-        // Init swiper
-        this.swiper = new Swiper(this.el, {
-            loop: true,
-            slidesPerView: 1,
-            centeredSlides: false,
-            spaceBetween: 30,
-            navigation: {
-                nextEl: this.ui.nextElement,
-                prevEl: this.ui.prevElement,
-            },
-            breakpoints: {
-                // when window width is >= 768px
-                768: {
-                    slidesPerView: 'auto',
-                    centeredSlides: true,
-                    spaceBetween: 80,
+        setTimeout(() => {
+            // Init swiper
+            this.swiper = new Swiper(this.el, {
+                // loop: true,
+                slidesPerView: 1,
+                centeredSlides: false,
+                spaceBetween: 30,
+                navigation: {
+                    nextEl: this.ui.nextElement,
+                    prevEl: this.ui.prevElement,
                 },
-            },
-        });
+                breakpoints: {
+                    // when window width is >= 768px
+                    768: {
+                        slidesPerView: 'auto',
+                        centeredSlides: true,
+                        spaceBetween: 80,
+                    },
+                },
+            })
+        }, 300)
     }
 
     onDestroy() {
