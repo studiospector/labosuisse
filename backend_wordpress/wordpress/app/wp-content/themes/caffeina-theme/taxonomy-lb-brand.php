7d390772b144468788b06478b3caf17f16ff47cb
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-02-09 10:50:33 +0100

    feat: add Brand page CPT for single brand

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/taxonomy-lb-brand.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/taxonomy-lb-brand.php
@@ -1,8 +1,11 @@
 <?php
 
+$brand_obj = get_queried_object();
+$brand_page = get_field('lb_brand_page', $brand_obj);
+
 $context = [
-    'title' => get_the_title(),
-    // 'content' => apply_filters( 'the_content', get_the_content() ),
+    'brand' => $brand_obj,
+    'content' => apply_filters( 'the_content', get_the_content(null, false, $brand_page) ),
 ];
 
 Timber::render('@PathViews/taxonomy-lb-brand.twig', $context);
