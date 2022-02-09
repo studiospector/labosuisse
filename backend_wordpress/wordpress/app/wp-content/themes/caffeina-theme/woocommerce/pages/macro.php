775047d74053ec758b43f6e5d4067c9fcd6f8b57
Matteo Meloni <matteo.meloni@caffeina.com>
2022-02-09 09:19:23 +0100

    feat: latest news

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/pages/macro.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/pages/macro.php
@@ -1,21 +1,70 @@
 <?php
-require_once(__DIR__.'/basePage.php');
+require_once(__DIR__ . '/basePage.php');
+
 use pages\basePage;
-class macro extends basePage  {
- 
-    public function __construct($name, $term){
-        parent::__construct('macro',$term);
-        $payload =  get_terms(array(
+
+class macro extends basePage
+{
+    private $levels = [
+        1 => 'Macro',
+        2 => 'Per Zona',
+        3 => 'Per Esigenza',
+        4 => 'Per Tipologia'
+    ];
+
+    public function __construct($name, $term)
+    {
+        parent::__construct('macro', $term);
+
+        $level = get_category_parents_custom($term->term_id);
+
+        $payload =  get_terms([
             'taxonomy' => 'product_cat',
             'hide_empty' => false,
-            'parent' => $term->term_id
-        ));
-    
+            'parent' => $term->term_id,
+        ]);
+
+        $items = [
+            'type' => 'submenu',
+            'label' => $term->name,
+            'groupDescription' => $this->levels[$level + 1],
+            'children' => $this->getSubItems($payload, $term->term_id)
+        ];
+
+        // die("<pre>" . json_encode($items, JSON_PRETTY_PRINT) . "</pre>");
+
         $this->setContext($payload);
-        //$this->macro->render();
-    }   
+        // $this->macro->render();
+    }
+
+
+    private function getSubItems($items, $parentId)
+    {
+        $menu = [];
+        foreach ($items as $i => $item) {
+
+            $level = get_category_parents_custom($item->term_id);
+
+            $menu[$i] = [
+                'type' => 'link',
+                'label' => $item->name,
+                'href' => get_term_link($item),
+            ];
+
+            $children = get_terms([
+                'taxonomy' => 'product_cat',
+                'hide_empty' => false,
+                'parent' => $item->term_id,
+            ]);
+
+            if (count($children)) {
+                $menu[$i]['type'] = 'submenu';
+                unset($menu[$i]['href']);
+                $menu[$i]['groupDescription'] = $this->levels[$level + 1];
+                $menu[$i]['children'] = $this->getSubItems($children, $item->term_id);
+            }
+        }
+
+        return $menu;
+    }
 }
-
-
-
-
