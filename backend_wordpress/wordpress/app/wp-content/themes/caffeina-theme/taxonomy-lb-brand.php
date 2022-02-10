b85bfdc19a14e6e7469ac9c064a57713417c1a1a
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-02-10 10:02:44 +0100

    fix: Brand pages CPT + Archive

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/taxonomy-lb-brand.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/taxonomy-lb-brand.php
@@ -1,11 +1,35 @@
 <?php
 
 $brand_obj = get_queried_object();
-$brand_page = get_field('lb_brand_page', $brand_obj);
+$level = get_category_parents_custom($brand_obj->term_id, 'lb-brand');
+
+$items = [];
+
+if ( have_posts() ) :
+    $num_posts = 0;
+    while ( have_posts() ) :
+        the_post();
+
+        $num_posts++;
+        $items[] = [
+            'product' => \Timber::get_post( get_the_ID() )
+        ];
+
+        if ($level == 1) {
+            
+        } else if ($level > 1) {
+            
+        }
+    endwhile;
+endif;
+
+wp_reset_postdata();
 
 $context = [
     'brand' => $brand_obj,
-    'content' => apply_filters( 'the_content', get_the_content(null, false, $brand_page) ),
+    'level' => $level,
+    'num_posts' => __('Risultati:', 'labo-suisse-theme') . ' <span>' . $num_posts . '</span>',
+    'items' => $items,
 ];
 
 Timber::render('@PathViews/taxonomy-lb-brand.twig', $context);
