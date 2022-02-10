3db48bc56f61d64fd352b633e706e777d1ddd349
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-02-10 20:31:19 +0100

    feat: add helper functions to get all Brands and Product Lines

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/helpers.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/helpers.php
@@ -1,6 +1,48 @@
 <?php
 
 /**
+ * Get all Brands
+ */
+function lb_get_brands()
+{
+    $brands = get_terms( array(
+        'taxonomy' => 'lb-brand',
+        'hide_empty' => false,
+    ));
+    
+    $i = 0;
+    foreach ($brands as $term) {
+        if ($term->parent != 0) {
+            unset($brands[$i]);
+        }
+        $i++;
+    }
+
+    return $brands;
+}
+
+/**
+ * Get all "Linee di prodotto"
+ */
+function lb_get_brands_product_lines()
+{
+    $product_lines = get_terms( array(
+        'taxonomy' => 'lb-brand',
+        'hide_empty' => false,
+    ));
+    
+    $i = 0;
+    foreach ($product_lines as $term) {
+        if ($term->parent == 0) {
+            unset($product_lines[$i]);
+        }
+        $i++;
+    }
+
+    return $product_lines;
+}
+
+/**
  * Get taxonomy term level
  */
 function get_category_parents_custom($category_id, $tax)
