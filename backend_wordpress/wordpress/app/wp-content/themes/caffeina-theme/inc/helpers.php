755e6ed52e03c03f59c39aa96f2a90bb323708c4
Matteo Meloni <matteo.meloni@caffeina.com>
2022-02-11 15:09:54 +0100

    fix: nginx configuration for prod

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/helpers.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/helpers.php
@@ -5,11 +5,11 @@
  */
 function lb_get_brands()
 {
-    $brands = get_terms( array(
+    $brands = get_terms(array(
         'taxonomy' => 'lb-brand',
         'hide_empty' => false,
     ));
-    
+
     $i = 0;
     foreach ($brands as $term) {
         if ($term->parent != 0) {
@@ -26,11 +26,11 @@ function lb_get_brands()
  */
 function lb_get_brands_product_lines()
 {
-    $product_lines = get_terms( array(
+    $product_lines = get_terms(array(
         'taxonomy' => 'lb-brand',
         'hide_empty' => false,
     ));
-    
+
     $i = 0;
     foreach ($product_lines as $term) {
         if ($term->parent == 0) {
@@ -56,4 +56,15 @@ function get_category_parents_custom($category_id, $tax)
     $parent_terms = get_term_parents_list($category_id, $tax, $args);
 
     return substr_count($parent_terms, ',');
+}
+
+function get_brands_menu()
+{
+    $payload =  get_terms(array(
+        'taxonomy' => 'lb-brand',
+        'hide_empty' => false,
+        'parent' => null
+    ));
+
+    // die(json_encode($payload, JSON_PRETTY_PRINT));
 }
