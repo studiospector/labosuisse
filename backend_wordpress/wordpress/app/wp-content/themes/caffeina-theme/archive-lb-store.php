038e24cd25102f113b933d9f6003c9d95d6dc6f3
Matteo Meloni <matteo.meloni@caffeina.com>
2022-03-11 10:44:42 +0100

    feat: add beauty specialist CPT + fields minor changes stores CPT

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/archive-lb-store.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/archive-lb-store.php
@@ -1,2 +1,24 @@
 <?php
 
+
+use Caffeina\LaboSuisse\Option\Option;
+use Timber\Timber;
+
+$options = (new Option())->getStoresOptions();
+
+$context = [
+    'title' => $options['title'],
+    'description' => $options['description'],
+    'infobox' => [
+        'infobox' => [
+            'image' => $options['infobox']['image'],
+            'tagline' => $options['infobox']['tagline'],
+            'title' => $options['infobox']['title'],
+            'subtitle' => $options['infobox']['subtitle'],
+            'paragraph' => $options['infobox']['paragraph'],
+            'cta' => $options['infobox']['cta'],
+        ]
+    ]
+];
+
+//Timber::render('@PathViews/', $context);
