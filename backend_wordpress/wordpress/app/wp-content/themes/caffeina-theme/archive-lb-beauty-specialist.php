6c305190b60a7d2d405e27495a2acda330e09ea8
Matteo Meloni <matteo.meloni@caffeina.com>
2022-03-15 11:58:26 +0100

    feat: filter beauty specialist

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/archive-lb-beauty-specialist.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/archive-lb-beauty-specialist.php
@@ -5,30 +5,35 @@ use Timber\Timber;
 $items = [];
 $today = date("Ymd",mktime(0,0,0,date("m"),date("d"),date("Y")));
 
-//$city = 'Raffadali';
-//$stores = new \WP_Query([
-//    'post_status' => 'publish',
-//    'post_type' => 'lb-store',
-//    'fields' => 'ids',
-//    'meta_query' => [
-//        [
-//            'key' => 'lb_stores_gmaps_point',
-//            'value' =>'"'.$city.'"',
-//            'compare' => 'LIKE'
-//        ]
-//    ]
-//]);
-//$stores = $stores->get_posts();
+
+$stores = [];
+
+if(isset($_GET['city'])) {
+    $stores = new \WP_Query([
+        'post_status' => 'publish',
+        'post_type' => 'lb-store',
+        'fields' => 'ids',
+        'meta_query' => [
+            [
+                'key' => 'lb_stores_gmaps_point',
+                'value' => '"' . $_GET['city'] . '"',
+                'compare' => 'LIKE'
+            ]
+        ]
+    ]);
+    $stores = $stores->get_posts();
+}
 
 $args = [
     'post_status' => 'publish',
     'post_type' => 'lb-beauty-specialist',
-//    'meta_query' => [[
-//        'key' => 'lb_beauty_specialist_store',
-//        'value' => $stores,
-//        'compare' => 'IN'
-//    ]]
+    'meta_query' => [[
+        'key' => 'lb_beauty_specialist_store',
+        'value' => $stores,
+        'compare' => 'IN'
+    ]]
 ];
+
 
 if(!isset($_GET['show_expired'])) {
     $args['meta_query'][] = [
@@ -45,6 +50,7 @@ foreach ($query->get_posts() as $post) {
     $date = get_field('lb_beauty_specialist_date', $post->ID);
 
     $items[$date][] = [
+        'store_id' => get_field('lb_beauty_specialist_store', $post->ID),
         'expired' => strtotime($date) < strtotime('now'),
         'date' => $date,
         'store' => $store->post_title,
@@ -58,6 +64,7 @@ wp_reset_postdata();
 $context = [
     'items' => $items,
 ];
+
 
 //Timber::render('@PathViews/', $context);
 
