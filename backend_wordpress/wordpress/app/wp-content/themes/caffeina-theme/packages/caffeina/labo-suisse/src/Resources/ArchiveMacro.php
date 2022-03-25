f9436d182155dcf93b580261e523d35235df6122
Matteo Meloni <matteo.meloni@caffeina.com>
2022-03-25 12:01:52 +0100

    feat: get all macro products

---
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/packages/caffeina/labo-suisse/src/Resources/ArchiveMacro.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Caffeina\LaboSuisse\Resources;
+
+use Timber\Timber;
+
+class ArchiveMacro
+{
+    private $category;
+
+    public $brands = [];
+    public $products = [];
+    public $totalPosts = 0;
+
+    /**
+     * @param $category
+     */
+    public function __construct($category)
+    {
+        $this->category = $category;
+
+        $this->products();
+    }
+
+    private function products()
+    {
+        foreach (lb_get_brands() as $brand) {
+            $query = new \WP_Query([
+                'post_type' => 'product',
+                'posts_per_page' => -1,
+                'tax_query' => $this->setFilter($brand)
+            ]);
+
+            if (empty($query->posts)) {
+                continue;
+            }
+
+            $this->brands[] = [
+                'value' => $brand->slug,
+                'label' => $brand->name,
+            ];
+
+            $this->products[$brand->term_id]['brand_card'] = $this->getBrandCard($brand);
+
+            foreach ($query->posts as $product) {
+                $this->products[$brand->term_id]['products'][] = Timber::get_post($product->ID);
+                $this->totalPosts++;
+            }
+        }
+
+        wp_reset_postdata();
+    }
+
+
+    /**
+     * @param $brand
+     * @return array
+     */
+    private function setFilter($brand)
+    {
+        $filters = [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'lb-brand',
+                'field' => 'slug',
+                'terms' => $brand->slug,
+            ]
+        ];
+
+        if ($this->category) {
+            $filters[] = [
+                'taxonomy' => 'product_cat',
+                'field' => 'slug',
+                'terms' => $this->category
+            ];
+        }
+
+        return $filters;
+    }
+
+    /**
+     * @param $brand
+     * @return array
+     */
+    public function getBrandCard($brand)
+    {
+        return [
+            'color' => get_field('lb_brand_color', $brand),
+            'infobox' => [
+                'subtitle' => $brand->name,
+                'paragraph' => $brand->description,
+                'cta' => [
+                    'url' => get_term_link($brand),
+                    'title' => __('Scopri il brand', 'labo-suisse-theme'),
+                    'variants' => ['quaternary']
+                ]
+            ],
+            'variants' => ['type-8']
+        ];
+    }
+}
--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-archive-macro.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-archive-macro.php
@@ -2,8 +2,35 @@
 
 /* Template Name: Template Archive Macro categoria */
 
+use Caffeina\LaboSuisse\Resources\ArchiveMacro;
+use Timber\Timber;
+
+$archiveMacro = new ArchiveMacro($_GET['product_category']);
+
 $context = [
-    
+    'brands_filter' => $archiveMacro->brands,
+    'tipologie_filter' => [] ,
+    'order_filter' => [],
+    'grid_type' => 'ordered',
+    'num_posts' => __('Risultati:', 'labo-suisse-theme') . ' <span>' . $archiveMacro->totalPosts . '</span>',
+    'items' => $archiveMacro->products,
+    'block_announcements' => (!get_field('lb_product_cat_announcements_state', $term)) ? null : [
+        'infobox' => [
+            'title' => get_field('lb_product_cat_announcements_title', $term),
+        ],
+        'cards' => [
+            [
+                'images' => lb_get_images(get_field('lb_product_cat_announcements_cardleft_image', $term)),
+                'infobox' => getCardByType(get_field('lb_product_cat_announcements_cardleft_item_type', $term), 'left', $term)
+            ],
+            [
+                'images' => lb_get_images(get_field('lb_product_cat_announcements_cardright_image', $term)),
+                'infobox' => getCardByType(get_field('lb_product_cat_announcements_cardright_item_type', $term), 'right', $term)
+            ]
+        ],
+        'variants' => ['horizontal'],
+    ]
 ];
 
 Timber::render('@PathViews/template-archive-macro.twig', $context);
+
