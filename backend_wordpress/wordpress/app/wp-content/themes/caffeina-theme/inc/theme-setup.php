1f55313e95babfe5899ba5d242387df751ce0406
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-03-30 14:58:13 +0200

    feat: posts per page product cat

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/theme-setup.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/theme-setup.php
@@ -377,6 +377,11 @@ function lb_post_filters($query)
         $query->set('posts_per_page', 12);
     }
 
+    // Product Category Archive page
+    if (is_tax('product_cat') && !is_admin() && $query->is_main_query() && !is_home() && !is_front_page()) {
+        $query->set('posts_per_page', 12);
+    }
+
     // Posts page
     if (is_home() && !is_admin() && $query->is_main_query() && !is_front_page() && !is_archive()) {
         $query->set('posts_per_page', 12);
