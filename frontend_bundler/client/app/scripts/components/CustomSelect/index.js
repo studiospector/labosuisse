2c6b320e88ed75ce654762d3c1f08e161e58b6b4
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-04-01 15:18:02 +0200

    fix: Custom selects

--- frontend_bundler/client/app/scripts/components/CustomSelect/index.js
+++ frontend_bundler/client/app/scripts/components/CustomSelect/index.js
@@ -1,4 +1,5 @@
 import Component from '@okiba/component'
+import { qs, on, off } from '@okiba/dom'
 
 import CustomSelect from '../../vendors/custom-select'
 
@@ -12,7 +13,21 @@ class LBCustomSelect extends Component {
         new CustomSelect({
             selector: this.el,
             // debug: true
-        });
+        })
+
+        this.customField = this.el.closest('.custom-select')
+        this.customFieldOptions = qs('.custom-select-items', this.customField)
+
+        on(this.customFieldOptions, 'mouseenter', this.disableLocomotive)
+        on(this.customFieldOptions, 'mouseleave', this.enableLocomotive)
+    }
+
+    disableLocomotive = (ev) => {
+        window.getCustomScrollbar.stop()
+    }
+
+    enableLocomotive = (ev) => {
+        window.getCustomScrollbar.start()
     }
 }
 
