b0dcc1c5b579c3b7572b0f546275f37db8562584
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-04-11 17:21:44 +0200

    fix: archive brands filters

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-lb-brand-archive.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-lb-brand-archive.php
@@ -6,16 +6,17 @@ $brands = [];
 $brand_terms = lb_get_brands();
 
 foreach ($brand_terms as $term) {
+    $brand_page = get_field('lb_brand_page', $term);
     $brands[] = [
         'images' => lb_get_images(get_field('lb_brand_image', $term)),
         'infobox' => [
             'subtitle' => $term->name,
             'paragraph' => $term->description,
-            'cta' => [
-                'url' => get_permalink( get_field('lb_brand_page', $term) ),
+            'cta' => !empty($brand_page) ? [
+                'url' => get_permalink($brand_page),
                 'title' => __('Vai al brand', 'labo-suisse-theme'),
                 'variants' => ['quaternary']
-            ]
+            ] : null
         ],
         'variants' => ['type-10']
     ];
@@ -24,6 +25,47 @@ foreach ($brand_terms as $term) {
 $context = [
     'title' => get_the_title(),
     'content' => apply_filters( 'the_content', get_the_content() ),
+    'filters' => [
+        'post_type' => 'product',
+        'posts_per_page' => -1,
+        'containerized' => false,
+        'items' => [
+            [
+                'id' => 'lb-product-cat-zona',
+                'label' => '',
+                'placeholder' => __('Zona', 'labo-suisse-theme'),
+                'multiple' => false,
+                'required' => false,
+                'disabled' => false,
+                'confirmBtnLabel' => __('Applica', 'labo-suisse-theme'),
+                'options' => [],
+                'attributes' => ['data-taxonomy="product_cat"'],
+                'variants' => ['primary']
+            ],
+            [
+                'id' => 'lb-product-cat-esigenza',
+                'label' => '',
+                'placeholder' => __('Esigenza', 'labo-suisse-theme'),
+                'multiple' => false,
+                'required' => false,
+                'disabled' => false,
+                'confirmBtnLabel' => __('Applica', 'labo-suisse-theme'),
+                'options' => [],
+                'attributes' => ['data-taxonomy="product_cat"'],
+                'variants' => ['primary']
+            ],
+        ],
+        'search' => [
+            'type' => 'search',
+            'name' => 'search',
+            'label' => __('Cerca', 'labo-suisse-theme'),
+            'value' => !empty($_GET['search']) ? $_GET['search'] : null,
+            'disabled' => false,
+            'required' => true,
+            'buttonTypeNext' => 'submit',
+            'variants' => ['secondary'],
+        ],
+    ],
     'items' => $brands,
 ];
 
