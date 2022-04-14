9d6c9c1ccef964d57eff4b47ab9f18704b73c017
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-04-14 09:42:56 +0200

    feat: add template distributor

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-distributor.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-distributor.php
@@ -2,4 +2,33 @@
 
 /* Template Name: Template Distributori */
 
+$context = [
+    'page_intro' => [
+        'title' => get_the_title(),
+        'description' => get_the_excerpt(),
+    ],
+    'filters' => [
+        'post_type' => 'lb-distributor',
+        'posts_per_page' => -1,
+        'containerized' => false,
+        'items' => [
+            [
+                'id' => 'lb-brand',
+                'label' => '',
+                'placeholder' => __('Brand', 'labo-suisse-theme'),
+                'multiple' => true,
+                'required' => false,
+                'disabled' => false,
+                'confirmBtnLabel' => __('Applica', 'labo-suisse-theme'),
+                'options' => [],
+                'attributes' => ['data-taxonomy="lb-brand"'],
+                'variants' => ['primary']
+            ],
+        ],
+        'search' => null,
+        'hidden' => null
+    ],
+    'content' => apply_filters('the_content', get_the_content()),
+];
 
+Timber::render('@PathViews/template-distributor.twig', $context);
