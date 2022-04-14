d761842f8e8f6f04536569d11079b58add286a07
Matteo Meloni <matteo.meloni@caffeina.com>
2022-04-14 11:16:09 +0200

    feat: distributor api endpoint

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/packages/caffeina/labo-suisse/src/Api/Distributor/Distributor.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/packages/caffeina/labo-suisse/src/Api/Distributor/Distributor.php
@@ -14,16 +14,51 @@ class Distributor
             'posts_per_page' => -1
         ]);
 
-        foreach ($query->get_posts() as $distributor) {
-            $distributors[] = [
-                'title' => $distributor->post_title,
-                'content' => $distributor->post_content,
-                'phone' => get_field('lb_distributor_phone', $distributor->ID),
-                'email' => get_field('lb_distributor_email', $distributor->ID),
-                'address' => get_field('lb_distributor_address', $distributor->ID),
+        foreach ($query->get_posts() as $post) {
+
+
+            $distributor = [
+                'title' => $post->post_title,
+                'content' => $post->post_content,
+                'phone' => get_field('lb_distributor_phone', $post->ID),
+                'email' => get_field('lb_distributor_email', $post->ID),
+                'address' => get_field('lb_distributor_address', $post->ID),
+                'brands' => $this->getBrands($post),
+                'links' => []
             ];
+
+            foreach (get_field('lb_distributor_links', $post->ID) as $link) {
+                $distributor['links'][] = [
+                    'label' => $link['lb_distributor_links_label'],
+                    'links' => $this->parseLinks($link['lb_distributor_links_list']),
+                ];
+            }
+
+            $distributors[] = $distributor;
         }
 
         return rest_ensure_response($distributors);
+    }
+
+    private function getBrands($post)
+    {
+        $brands = wp_get_post_terms($post->ID, 'lb-brand', ['parent' => null]);
+
+        return array_map(function ($item) {
+            return [
+                'id' => $item->term_id,
+                'name' => $item->name
+            ];
+        }, $brands);
+    }
+
+    private function parseLinks($links)
+    {
+        return array_map(function ($item) {
+            return [
+                'icon' => $item['lb_distributor_links_list_icon'],
+                'link' => $item['lb_distributor_links_list_link']
+            ];
+        }, $links);
     }
 }
