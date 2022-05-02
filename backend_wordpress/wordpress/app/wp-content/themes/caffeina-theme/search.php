75fdfe93229a9e704fe254dc06a6739585136f3d
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-05-02 15:14:36 +0200

    fix: search page

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/search.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/search.php
@@ -4,12 +4,16 @@
 
 use Caffeina\LaboSuisse\Api\GlobalSearch\Search;
 
-$search_val = !empty($_GET['lb-search-val']) ? $_GET['lb-search-val'] : '';
+$search_val = !empty($_GET['lb-search-val']) ? $_GET['lb-search-val'] : null;
 
 $items = (new Search())
     ->setSearch($search_val)
     ->get();
 
-$context['items'] = $items;
+$context = [
+    'num_res' => 0,
+    'search_val' => $search_val,
+    'tabs' => $items,
+];
 
 Timber::render('@PathViews/search.twig', $context);
