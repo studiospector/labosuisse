4a8123fbdb3b1ceab15010ad342c1064c66ef46a
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-05-04 10:48:04 +0200

    feat: add 404 page

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/404.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/404.php
@@ -2,8 +2,17 @@
 
 header("HTTP/1.0 404 Not Found");
 
-$context = [];
+$context = [
+    'infobox' => [
+        'tagline' => __('Errore 404', 'labo-suisse-theme'),
+        'subtitle' => __('Ci dispiace, non riusciamo a trovare<br>la pagina che stai cercando. ', 'labo-suisse-theme'),
+        'paragraph' => __('Verifica l’indirizzo e riprova, oppure clicca sul pulsante<br>sotto per continuare a navigare sul nostro sito.', 'labo-suisse-theme'),
+        'cta' =>[
+            'title' => __('Torna alla Homepage', 'labo-suisse-theme'),
+            'url' => home_url(),
+            'variants' => ['primary']
+        ],
+    ]
+];
 
-echo "404";
-
-//Timber::render('@PathViews/', $context);
+Timber::render('@PathViews/404.twig', $context);
