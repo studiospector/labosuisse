f6b4c8616a410574aed1b6bfb553233829635ffb
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-05-17 13:19:03 +0200

    feat: add Offset nav to Footer newsletter link

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/helpers.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/helpers.php
@@ -320,6 +320,25 @@ function lb_footer()
 }
 
 /**
+ * Newsletter nav
+ */
+function lb_newsletter_nav()
+{
+    $footer_newsletter_options = get_field('lb_footer_newsletter', 'option');
+
+    return [
+        'id' => 'lb-newsletter-nav',
+        'title' => __('Iscriviti alla newsletter', 'labo-suisse-theme'),
+        'data' => [
+            [
+                'type' => 'html',
+                'data' => do_shortcode($footer_newsletter_options['lb_footer_newsletter_shortcode'])
+            ]
+        ]
+    ];
+}
+
+/**
  * Check if current user has role
  */
 function lb_user_has_role($role)
