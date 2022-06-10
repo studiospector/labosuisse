7f55f778db370788e69ee0baa26179f353f74c21
Stefano Iachetta <stefano.iachetta@caffeina.com>
2022-06-10 15:30:28 +0200

    fix: add link to Product card

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/woocommerce-setup.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/inc/woocommerce-setup.php
@@ -11,7 +11,8 @@ add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 
-
+remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
 
 /**
  * WC Support
