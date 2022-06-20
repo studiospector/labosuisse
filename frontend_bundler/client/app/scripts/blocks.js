897691122290af52596629ba66db96fd68e83f2d
Stefano Iachetta <stefano.iachetta@caffeina.com>
2022-06-20 12:21:56 +0100

    fix: refactoring + add class to file gutenberg block

--- frontend_bundler/client/app/scripts/blocks.js
+++ frontend_bundler/client/app/scripts/blocks.js
@@ -5,31 +5,18 @@ import Application from '~/components/app'
 const lbSetExtraPropsToBlockType = (props, blockType, attributes) => {
     const notDefined = (typeof props.className === 'undefined' || !props.className) ? true : false
 
-    if (blockType.name === 'core/heading') {
+    console.log(blockType.name);
+
+    if (
+        blockType.name === 'core/heading'
+        || blockType.name === 'core/paragraph'
+        || blockType.name === 'core/list'
+        || blockType.name === 'core/image'
+        || blockType.name === 'core/file'
+    ) {
         return Object.assign(props, {
             // className: notDefined ? `post__heading-${props.tagName}` : `post__heading-${props.tagName} ${props.className}`,
             className: notDefined ? `lb-post-content` : `lb-post-content ${props.className}`,
-        })
-    }
-
-    if (blockType.name === 'core/paragraph') {
-        return Object.assign(props, {
-            // className: notDefined ? 'post__paragraph' : `post__paragraph ${props.className}`,
-            className: notDefined ? 'lb-post-content' : `lb-post-content ${props.className}`,
-        })
-    }
-
-    if (blockType.name === 'core/list') {
-        return Object.assign(props, {
-            // className: notDefined ? 'post__paragraph' : `post__paragraph ${props.className}`,
-            className: notDefined ? 'lb-post-content' : `lb-post-content ${props.className}`,
-        })
-    }
-
-    if (blockType.name === 'core/image') {
-        return Object.assign(props, {
-            // className: notDefined ? 'post__paragraph' : `post__paragraph ${props.className}`,
-            className: notDefined ? 'lb-post-content' : `lb-post-content ${props.className}`,
         })
     }
 
@@ -39,7 +26,7 @@ const lbSetExtraPropsToBlockType = (props, blockType, attributes) => {
 window.addEventListener('load', (event) => {
     wp.hooks.addFilter(
         'blocks.getSaveContent.extraProps',
-        'your-namespace/block-filters',
+        'labo-suisse-theme/block-filters',
         lbSetExtraPropsToBlockType
     )
 
@@ -48,11 +35,3 @@ window.addEventListener('load', (event) => {
 
     new Application()
 })
-
-// function init() {
-//   console.log('initAPP')
-//   return new Application()
-// }
-
-// if (!window.APP) window.APP = {}
-// window.APP.init = init
