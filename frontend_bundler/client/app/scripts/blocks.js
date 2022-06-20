0bf4a7561a4ad3a5651d810da7fca5cb258b4d48
Stefano Iachetta <stefano.iachetta@caffeina.com>
2022-06-20 15:53:10 +0100

    fix: remove custom blocks classes from Jobs

--- frontend_bundler/client/app/scripts/blocks.js
+++ frontend_bundler/client/app/scripts/blocks.js
@@ -5,8 +5,6 @@ import Application from '~/components/app'
 const lbSetExtraPropsToBlockType = (props, blockType, attributes) => {
     const notDefined = (typeof props.className === 'undefined' || !props.className) ? true : false
 
-    console.log(blockType.name);
-
     if (
         blockType.name === 'core/heading'
         || blockType.name === 'core/paragraph'
@@ -24,11 +22,25 @@ const lbSetExtraPropsToBlockType = (props, blockType, attributes) => {
 }
 
 window.addEventListener('load', (event) => {
-    wp.hooks.addFilter(
-        'blocks.getSaveContent.extraProps',
-        'labo-suisse-theme/block-filters',
-        lbSetExtraPropsToBlockType
-    )
+    let postType = null
+    const body = document.querySelector('body')
+
+    body.classList.forEach((attr) => {
+        if ('post-type-' === attr.slice(0, 10)) {
+            postType = attr.split('post-type-')
+            postType = postType[postType.length - 1]
+
+            return
+        }
+    })
+
+    if (postType != 'lb-job') {
+        wp.hooks.addFilter(
+            'blocks.getSaveContent.extraProps',
+            'labo-suisse-theme/block-filters',
+            lbSetExtraPropsToBlockType
+        )
+    }
 
     // Disable default Separator block
     wp.blocks.unregisterBlockType('core/separator')
