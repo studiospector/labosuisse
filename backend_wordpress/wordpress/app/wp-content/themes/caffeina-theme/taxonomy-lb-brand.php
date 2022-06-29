86e2c38312f1ad50c0e8540153c22dfbadd65851
Matteo Meloni <matteo.meloni@caffeina.com>
2022-06-29 12:09:48 +0200

    chore: CR taxonomy-lb-brand

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/taxonomy-lb-brand.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/taxonomy-lb-brand.php
@@ -1,45 +1,27 @@
 <?php
 
 $brand_obj = get_queried_object();
-$level = get_category_parents_custom($brand_obj->term_id, 'lb-brand');
 
 $items = [];
-$grid_type = 'default';
 
 if ( have_posts() ) :
-    $num_posts = 0;
     while ( have_posts() ) :
         the_post();
 
-        $num_posts++;
-
-        if ($level == 1) {
-            $grid_type = 'default';
-
-            $terms = get_the_terms( get_the_ID(), 'lb-brand' );
-            
-            foreach ($terms as $term) {
-                if ( $term->parent != 0 ) {
-                    $items[] = \Timber::get_post( get_the_ID() );
-                }
-            }
-
-        } else if ($level > 1) {
-            $items[] = \Timber::get_post( get_the_ID() );
-        }
+        $items[] = \Timber::get_post( get_the_ID() );
     endwhile;
 endif;
 
 wp_reset_postdata();
 
+
 $context = [
-    'level' => $level,
     'page_intro' => [
         'title' => $brand_obj->name,
         'description' => nl2br($brand_obj->description),
     ],
-    'num_posts' => __('Risultati:', 'labo-suisse-theme') . ' <span>' . $num_posts . '</span>',
-    'grid_type' => $grid_type,
+    'num_posts' => __('Risultati:', 'labo-suisse-theme') . ' <span>' . $brand_obj->count . '</span>',
+    'grid_type' => 'default',
     'items' => $items,
     'pagination' => lb_pagination(),
 ];
