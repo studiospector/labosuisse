43cc1b1ff6825d7535553b8cf62724272b9ba5da
Stefano Iachetta <stefano.iachetta@caffeina.com>
2022-09-06 14:01:38 +0200

    feat: add custom email template WC

---
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/emails/email-header.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Email Header
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-header.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 4.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
+		<title><?php echo get_bloginfo( 'name', 'display' ); ?></title>
+	</head>
+	<body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+		<div id="wrapper" dir="<?php echo is_rtl() ? 'rtl' : 'ltr'; ?>">
+			<table border="0" cellpadding="0" cellspacing="0" width="100%">
+				<tr>
+					<td align="center" valign="top">
+						<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container">
+							<tr>
+								<td align="center" valign="top" class="lb-header">
+									<!-- Logos -->
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
+										<tr class="lb-header__logos">
+											<td class="lb-header__logos__patent">
+												<img src="https://static.labosuisse.com/uploads/2022/09/05162834/email-patent.png" alt="Patent">
+											</td>
+											<td class="lb-header__logos__logo">
+												<img src="https://static.labosuisse.com/uploads/2022/09/05162836/email-logo.png" alt="Logo">
+											</td>
+										</tr>
+									</table>
+									<!-- End Logos -->
+									<!-- Header -->
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
+										<tr>
+											<td id="header_wrapper">
+												<div class="lb-header__banner" style="background-image: url('https://static.labosuisse.com/uploads/2022/09/05170359/email-banner.jpg');">
+													<h1><?php echo $email_heading; ?></h1>
+												</div>
+											</td>
+										</tr>
+									</table>
+									<!-- End Header -->
+								</td>
+							</tr>
+							<tr>
+								<td align="center" valign="top">
+									<!-- Body -->
+									<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
+										<tr>
+											<td valign="top" id="body_content">
+												<!-- Content -->
+												<table border="0" cellpadding="20" cellspacing="0" width="100%">
+													<tr>
+														<td valign="top">
+															<div id="body_content_inner">
