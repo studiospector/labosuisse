dbcc8fc0b0bf5b71be1d21f114049a61d456ca4d
Stefano Iachetta <stefano.iachetta@caffeina.com>
2022-09-15 15:16:52 +0200

    feat: geolocation test

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/packages/caffeina/labo-suisse/src/Api/Multicountry/Geolocation.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/packages/caffeina/labo-suisse/src/Api/Multicountry/Geolocation.php
@@ -6,7 +6,9 @@ class Geolocation
 {
     public function get()
     {
+        $geolocate = $this->geolocate();
         $response = [
+            'geolocation' => $geolocate,
             'infobox' => [
                 'paragraph' => "Looks like you're in Spain!<br>You want to visit the Labo Suisse International Website?"
             ],
@@ -27,5 +29,20 @@ class Geolocation
         ];
 
         return $response;
+    }
+
+    private function geolocate()
+    {
+        $wc_geo_instance  = new \WC_Geolocation();
+        $user_ip = $wc_geo_instance->get_ip_address();
+        $user_geodata_wc = $wc_geo_instance->geolocate_ip($user_ip);
+
+        $maxmind_geo_instance  = new \WC_Integration_MaxMind_Geolocation();
+        $user_geodata_maxmind = $maxmind_geo_instance->get_geolocation(null, $user_ip);
+        
+        return [
+            'wc' => $user_geodata_wc,
+            'maxmind' => $user_geodata_maxmind,
+        ];
     }
 }
