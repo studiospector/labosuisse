08c02d4b28de013d405c52e9582a7ee8422fb103
Stefano Iachetta <stefano.iachetta@caffeina.com>
2022-09-21 10:50:08 +0200

    gix: row in form checkout

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/checkout/form-checkout.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/woocommerce/checkout/form-checkout.php
@@ -29,41 +29,43 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 ?>
 
-<form name="checkout" method="post" class="lb-wc-checkout lb-wc-custom-fields checkout woocommerce-checkout row" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+<form name="checkout" method="post" class="lb-wc-checkout lb-wc-custom-fields checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
-    <div class="col-12 col-lg-8">
-        <?php if ( $checkout->get_checkout_fields() ) : ?>
+    <div class="row">
+        <div class="col-12 col-lg-8">
+            <?php if ( $checkout->get_checkout_fields() ) : ?>
 
-            <?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+                <?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-            <div class="" id="customer_details">
-                <div class="">
-                    <?php do_action( 'woocommerce_checkout_billing' ); ?>
+                <div class="" id="customer_details">
+                    <div class="">
+                        <?php do_action( 'woocommerce_checkout_billing' ); ?>
+                    </div>
+
+                    <div class="">
+                        <?php do_action( 'woocommerce_checkout_shipping' ); ?>
+                    </div>
                 </div>
 
-                <div class="">
-                    <?php do_action( 'woocommerce_checkout_shipping' ); ?>
-                </div>
-            </div>
+                <?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-            <?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
-        <?php endif; ?>
-        
-        <?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-    </div>
-	
-    <div class="col-12 col-lg-4">
-        <div class="lb-wc-checkout__details">
-            <h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+            <?php endif; ?>
             
-            <?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+            <?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+        </div>
         
-            <div id="order_review" class="woocommerce-checkout-review-order">
-                <?php do_action( 'woocommerce_checkout_order_review' ); ?>
+        <div class="col-12 col-lg-4">
+            <div class="lb-wc-checkout__details">
+                <h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+                
+                <?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+            
+                <div id="order_review" class="woocommerce-checkout-review-order">
+                    <?php do_action( 'woocommerce_checkout_order_review' ); ?>
+                </div>
+            
+                <?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
             </div>
-        
-            <?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
         </div>
     </div>
 
