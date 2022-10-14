446884b433bee913600f2a2980bff6639f9aad84
Matteo Meloni <matteo.meloni@caffeina.com>
2022-10-14 15:42:20 +0200

    migrate to gitlab managed

--- cmd/pull.sh
+++ cmd/pull.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for image in $(docker images -f "reference=registry.caffeina.co/devops/nador-images/*/*/*" --format {{.Repository}}); do docker pull $image; done
+for image in $(docker images -f "reference=registry.gitlab.com/never-sleep/devops/nador-images/*/*/*" --format {{.Repository}}); do docker pull $image; done
