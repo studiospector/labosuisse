ba7736694eec192c9b48049d3622a611074c552f
Stefano Iachetta <stefano.iachetta@caffeina.com>
2022-10-19 16:34:10 +0200

    feat: add hidden h1 for blank page template

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-blank-page.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/template-blank-page.php
@@ -3,6 +3,7 @@
 /* Template name: Template Blank Page */
 
 $context = [
+    'page_title' => get_field('lb_page_blank_heading_h1'),
     'content' => apply_filters('the_content', get_the_content()),
 ];
 
