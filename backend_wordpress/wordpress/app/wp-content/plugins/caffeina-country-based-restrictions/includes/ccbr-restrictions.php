a7d6929445f00c929e6ba3227a26bb033f30bd1b
Stefano Iachetta <stefano.iachetta@hotmail.it>
2022-11-11 18:51:40 +0100

    feat: add plugin Caffeina Country Based Restrictions

---
+++ backend_wordpress/wordpress/app/wp-content/plugins/caffeina-country-based-restrictions/includes/ccbr-restrictions.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * CCBR_Restrictions 
+ *
+ * @class   CCBR_Restrictions
+ * @package WooCommerce/Classes
+ */
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+/**
+ * CCBR_Restrictions class
+ *
+ * @since  1.0
+ */
+class CCBR_Restrictions
+{
+	/**
+	 * Instance of this class.
+	 *
+	 * @since  1.0
+	 * @var object Class Instance
+	 */
+	private static $instance;
+
+	/**
+	 * Get the class instance
+	 *
+	 * @since  1.0
+	 * @return CCBR_Restrictions
+	 */
+	public static function get_instance()
+	{
+		if (null === self::$instance) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Initialize the main plugin function
+	 * 
+	 * @since  1.0
+	 */
+	public function __construct()
+	{
+		$this->init();
+	}
+
+	/**
+	 * Init function
+	 *
+	 * @since  1.0
+	 */
+	public function init()
+	{
+		// Callback on activate plugin
+		register_activation_hook(__FILE__, array($this, 'on_activation'));
+
+		// Hook for geolocation_update_database	
+		add_filter('woocommerce_maxmind_geolocation_update_database_periodically', array($this, 'update_geo_database'), 10, 1);
+
+		add_filter('woocommerce_is_purchasable', array($this, 'is_purchasable'), 100, 2);
+		add_filter('woocommerce_variation_is_purchasable', array($this, 'is_purchasable'), 100, 2);
+	}
+
+	/**
+	 * WC_Geolocation database update hooks
+	 *
+	 * @since 1.0
+	 *
+	 */
+	function on_activation()
+	{
+		WC_Geolocation::update_database();
+	}
+
+	/**
+	 * Update geo database
+	 *
+	 * @since 1.0
+	 */
+	function update_geo_database()
+	{
+		return true;
+	}
+
+	/**
+	 * Check product is_purchasable or not
+	 *
+	 * @since  1.0
+	 */
+	public function is_purchasable($is_purchasable, $product)
+	{
+		if (!is_admin()) {
+			global $sitepress;
+	
+			// Current site lang
+			$current_lang = $sitepress->get_current_language();
+			// Product lang
+			$wpml_product_details = apply_filters('wpml_post_language_details', null, $product->get_id());
+	
+			// TODO: Check on user geolocation with countries available on specific catalog
+			// Check for IT Catalog => Countries -> IT
+			if ($current_lang == 'it' && ($current_lang == $wpml_product_details['language_code'])) {
+				return false;
+			// Check for EN Catalog => Countries -> BE, IT, FR, IE, ES, NL, DE
+			} elseif ($current_lang == 'en' && ($current_lang == $wpml_product_details['language_code'])) {
+				return false;
+			}
+		}
+
+		return $is_purchasable;
+	}
+}
