7d8cef241bf27e9d0d0bd85f56d3a4770fab3a60
Stefano Iachetta <stefano.iachetta@hotmail.it>
2023-03-01 16:31:07 +0100

    fix: Video block

--- frontend_bundler/client/app/scripts/components/Video/index.js
+++ frontend_bundler/client/app/scripts/components/Video/index.js
@@ -2,21 +2,27 @@ import Component from "@okiba/component";
 import Plyr from "plyr";
 
 const ui = {
-  video: "[data-video]",
+  video: ".lb-video__player",
 };
 
 class Video extends Component {
   constructor({ options, ...props }) {
     super({ ...props, ui });
 
+    console.log('this.ui.video', this.ui.video);
+
     if (this.ui.video) {
       const player = new Plyr(this.ui.video);
 
-      player.on("ended", (event) => {
-        const instance = event.detail.plyr;
-        instance.restart();
-        setTimeout(() => instance.pause(), 100);
-      });
+      console.log("player", player);
+
+      if (player) {
+        player.on("ended", (event) => {
+          const instance = event.detail.plyr;
+          instance.restart();
+          setTimeout(() => instance.pause(), 100);
+        });
+      }
     }
   }
 }
