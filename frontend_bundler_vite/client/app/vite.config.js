9868033aa2ce5666afbd42a27ee8ecb82d4074cb
Stefano Iachetta <stefano.iachetta@hotmail.it>
2023-07-05 09:17:07 +0200

    chore: minify after generate bundle with terser

--- frontend_bundler_vite/client/app/vite.config.js
+++ frontend_bundler_vite/client/app/vite.config.js
@@ -1,26 +1,64 @@
-import { defineConfig } from 'vite'
-
 import { resolve } from 'node:path'
-
+import { defineConfig } from 'vite'
+import { minify } from 'terser'
 import autoprefixer from 'autoprefixer'
+
+function minifyBundles() {
+  return {
+    name: 'minifyBundles',
+
+    // async writeBundle(options, bundle) {
+    //   for (let key in bundle) {
+    //     if (bundle[key].type == 'chunk' && key.endsWith('.js')) {
+    //       console.log(key, bundle[key].type);
+    //       const minifyCode = await minify(bundle[key].code, { sourceMap: false })
+    //       bundle[key].code = minifyCode.code
+    //     }
+    //   }
+    //   return bundle
+    // },
+
+    // async transform(code, id) {
+    //   let result = code
+    //   if (id.endsWith('.js')) {
+    //     const minifyCode = await minify(code, { sourceMap: false })
+    //     result = minifyCode.code
+    //   }
+    //   return result
+    // },
+
+    async generateBundle(options, bundle) {
+      for (let key in bundle) {
+        if (bundle[key].type == 'chunk' && key.endsWith('.js')) {
+          const minifyCode = await minify(bundle[key].code, {
+            compress: true,
+            mangle: true,
+            sourceMap: false,
+          })
+          bundle[key].code = minifyCode.code
+        }
+      }
+      return bundle
+    },
+  }
+}
 
 export default defineConfig({
   appType: 'custom',
   css: {
     devSourcemap: true,
     postcss: {
-      plugins: [
-        autoprefixer
-      ],
-    }
+      plugins: [autoprefixer],
+    },
   },
   build: {
     target: ['es2015'],
     outDir: 'dist',
     emptyOutDir: true,
     cssCodeSplit: true,
-    sourcemap: process.env.NODE_ENV == 'development' ? true : false,
+    sourcemap: false,
     lib: {
+      formats: ['es'],
       entry: [
         resolve(__dirname, 'src/scripts/main.js'),
         resolve(__dirname, 'src/scripts/critical.js'),
@@ -30,16 +68,20 @@ export default defineConfig({
       ],
       fileName: '[name]',
     },
-    watch: (process.env.NODE_ENV != 'development') ? null : {
-      exclude: 'node_modules/**',
-      include: 'src/**',
-    },
+    watch:
+      process.env.NODE_ENV != 'development'
+        ? null
+        : {
+            exclude: 'node_modules/**',
+            include: 'src/**',
+          },
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
-          return assetInfo.name;
+          return assetInfo.name
         },
       },
     },
   },
+  plugins: [minifyBundles()],
 })
