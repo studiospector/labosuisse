1d705145439fbc5d7727d650e787e76b08874798
Matteo Meloni <matteo.meloni@caffeina.com>
2023-07-25 12:25:47 +0200

    redirect single brand page

--- backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/single-lb-brand-page.php
+++ backend_wordpress/wordpress/app/wp-content/themes/caffeina-theme/single-lb-brand-page.php
@@ -4,4 +4,10 @@ $context = [
     'content' => apply_filters( 'the_content', get_the_content() ),
 ];
 
+$toRedirect = toRedirect();
+
+if(toRedirect()) {
+    wp_redirect($toRedirect, 301);
+}
+
 Timber::render('@PathViews/single-lb-brand-page.twig', $context);
