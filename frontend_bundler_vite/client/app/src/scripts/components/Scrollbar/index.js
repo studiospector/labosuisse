fd581f3f9d76e4b03231e03638d31f2aa86eaa22
Stefano Iachetta <stefano.iachetta@hotmail.it>
2023-11-17 11:52:56 +0100

    fix: scrollbar lenis var

--- frontend_bundler_vite/client/app/src/scripts/components/Scrollbar/index.js
+++ frontend_bundler_vite/client/app/src/scripts/components/Scrollbar/index.js
@@ -25,7 +25,7 @@ class Scrollbar extends Component {
         this.lenis.on('scroll', ScrollTrigger.update)
 
         gsap.ticker.add((time)=>{
-            lenis.raf(time * 1000)
+            this.lenis.raf(time * 1000)
         })
 
         gsap.ticker.lagSmoothing(0)
